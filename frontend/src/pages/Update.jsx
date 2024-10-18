0e1c0ac11f4884faea0e61e498efecbee7bba19d
MG04 <mariosgoura2@gmail.com>
2024-10-18 20:55:37 +0300

    Update Update.jsx

--- frontend/src/pages/Update.jsx
+++ frontend/src/pages/Update.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import React, { useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 
-const Update = async () => {
+const Update = () => {
   const [book, setBook] = useState({
     title: "",
     description: "",
